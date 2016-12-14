85cf4213e17d0e624c8babf3e704cec54ca6fc6e
wints <mwints@gmail.com>
2016-12-14 16:16:41 +0100

    Some more navbar fixes

--- js/stickysidebar.js
+++ js/stickysidebar.js
@@ -1,25 +1,38 @@
 
 $(document).ready(function(){
 
-    var sidebarHeight = $('#sidebar').height();
-    var windowHeight = $(window).height();
-    var sidebarOverflow = sidebarHeight - windowHeight;
+  var sbHeight = $('#sidebar').height();
 
-    var $sidebar   = $("#sidebar .navbar"), 
-    $window    = $(window),
-    footer = $('.footer').height();
+  function stickSidebar() {    
+    var $el = $('.footer'),
+        scrollTop = $(this).scrollTop(),
+        scrollBot = scrollTop + $(this).height(),
+        elTop = $el.offset().top,
+        elBottom = elTop + $el.outerHeight(),
+        visibleTop = elTop < scrollTop ? scrollTop : elTop,
+        visibleBottom = elBottom > scrollBot ? scrollBot : elBottom,
+        wHeight = $(this).height(),
+        sidebarOverflow = sbHeight - wHeight,
+        $sidebar   = $("#sidebar .navbar"),
+        distance  = (elTop - scrollTop);
+      
+    if (wHeight < sbHeight) {
 
-  if($(window).width() > 767) {
-
-    $window.scroll(function() {
-      if ($window.scrollTop() > sidebarOverflow) {
-        $sidebar.css({'position':'fixed','bottom':footer + 50 });
+      if (scrollTop > sidebarOverflow && wHeight < distance) {
+        $sidebar.css({'position':'fixed','bottom':0 });
+      } else if (wHeight >= distance) {
+        $sidebar.css({'position':'fixed','bottom':visibleBottom - visibleTop});
       } else {
         $sidebar.css({'position':'static'});
       }
-    });
+
+    } else if (wHeight > sbHeight){
+      $sidebar.css({'position':'fixed', 'top':0});
+    }
 
   }
+
+$(window).on('scroll resize', stickSidebar);
 
 
 });
